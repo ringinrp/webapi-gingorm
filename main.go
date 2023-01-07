4c227f43cb5255578d644eb2dc76372d6c6b1bf0
Ringin Restu Pati <ringinrp21@gmail.com>
2023-01-07 17:52:20 +0700

    first init

--- main.go
+++ main.go
@@ -1,2 +1,45 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", routeHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id", booksHandler)
+	router.GET("/query", queryHandler)
+
+	router.Run(":8000")
+
+}
+
+func routeHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Ringin Restu Pati",
+		"bio":  "Software Engineer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Restu Pati",
+		"bio":  "Information System",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func queryHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
