77397eeab89119579c6726cc8a538c5053031740
Ringin Restu Pati <ringinrp21@gmail.com>
2023-01-08 14:27:54 +0700

    update: error validation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 )
 
 func main() {
@@ -13,10 +15,15 @@ func main() {
 	router.GET("/hello", helloHandler)
 	router.GET("/books/:id", booksHandler)
 	router.GET("/query", queryHandler)
-	router.POST("/query", queryHandler)
+	router.POST("/books", postBooksHandler)
 
 	router.Run(":8000")
 
+}
+
+type BookInput struct {
+	Title string `json:"title" binding:"required"`
+	Price int    `json:"price" binding:"required,number"`
 }
 
 func routeHandler(c *gin.Context) {
@@ -37,6 +44,27 @@ func booksHandler(c *gin.Context) {
 	id := c.Param("id")
 
 	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+func postBooksHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on filed %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"Title": bookInput.Title,
+		"Price": bookInput.Price,
+	})
 }
 
 func queryHandler(c *gin.Context) {
